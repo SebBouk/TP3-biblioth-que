c95b37ca79d15f20709b55d814dc746a0fe62b3e
Sébastien <seb.boukobza@gmail.com>
2024-03-25 11:14:27 +0100

    rute

--- project-root/app/Controllers/Connection.php
+++ project-root/app/Controllers/Connection.php
@@ -11,7 +11,7 @@ class Connection extends BaseController
     {
         $values = $this->request->getPost(['login', 'password']);
         if (!empty($values) && $values['login'] == APP_ADMIN_LOGIN &&$values['password'] == APP_ADMIN_PASSWORD) {
-            return "On a réussi à se connecter !";
+            return redirect()->route('/');
         } else {
             return "On a pas réussi à se connecter !";
         }
