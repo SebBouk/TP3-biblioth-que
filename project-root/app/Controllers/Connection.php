523fe8b70d9f8544dabaa55383d94eb972fc44c5
Sébastien <seb.boukobza@gmail.com>
2024-03-25 10:38:24 +0100

    attempt

--- project-root/app/Controllers/Connection.php
+++ project-root/app/Controllers/Connection.php
@@ -6,5 +6,9 @@ class Connection extends BaseController
     public function index(): string
     {
         return view("login_form");
-}
+    }
+    public function attemptLogin(): string
+    {
+        return "On essaie de se connecter !";
+    }
 }
