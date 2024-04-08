d90635be899f6f868b0d5cbb5ca51437effaca13
peach31270 <thomas.fougeras31@gmail.com.com>
2024-04-08 12:08:44 +0200

    ok

--- project-root/app/Controllers/Home.php
+++ project-root/app/Controllers/Home.php
@@ -4,10 +4,11 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index():
+    public function index()
     {
        $template =
-       view('templateHeader.php').
-       view('login_form.php');
+       view('template/templateHeader.php').
+       view('login_form.php').
+       view('template/templateFooter.php');
     }
 }
