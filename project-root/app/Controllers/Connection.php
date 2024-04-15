71db3d33f8cfcc3aafb3d249dfbb9e3679c9f2a1
peach31270 <thomas.fougeras31.com>
2024-04-15 12:27:17 +0200

    ok

--- project-root/app/Controllers/Connection.php
+++ project-root/app/Controllers/Connection.php
@@ -10,12 +10,19 @@ class Connection extends BaseController
     }
     public function attemptLogin()
     {
+        $abonneModel = new \App\Models\Abonne();
         $values = $this->request->getPost(['login', 'password']);
         if (!empty($values) && $values['login'] == APP_ADMIN_LOGIN && $values['password'] == APP_ADMIN_PASSWORD) {
             // return view('welcome_message');
             return redirect()->to('/home');
         } else {
             return "On a pas réussi à se connecter !";
+        }
+        $rechercheAbonne = $abonneModel->getAbonneByMatricule($values['login']);
+        if (isset($rechercheAbonne) && $rechercheAbonne['nom_abonne'] === $values['password'])
+            return redirect()->to("home");
+        else{
+            return redirect()->to('login');
         }
     }
 }
