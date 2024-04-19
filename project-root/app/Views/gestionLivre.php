b270bffe76c2223d1cb01d8524338fcfc9770fa9
Sébastien <seb.boukobza@gmail.com>
2024-04-19 14:06:42 +0200

    gestion livres

--- project-root/app/Views/gestionLivre.php
+++ project-root/app/Views/gestionLivre.php
@@ -1,3 +1,19 @@
 <body>
     <h1>Bienvenue sur la page de gestion des livres</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Titre</th>
+                <th>Theme</th>
+            </tr>
+        </thead>
+        <tbody>
+                <?php foreach ($livres as $livre) : ?>
+            <tr>
+                <td><?php echo $livre['titre_livre']; ?></td>
+                <td><?php echo $livre['theme_livre']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
