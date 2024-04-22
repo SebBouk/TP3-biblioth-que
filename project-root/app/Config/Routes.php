57432186b04f1ad626b814b2ca3613c77fe4cdca
Sébastien <seb.boukobza@gmail.com>
2024-04-22 10:44:26 +0200

    filtre

--- project-root/app/Config/Routes.php
+++ project-root/app/Config/Routes.php
@@ -10,5 +10,5 @@ $routes->get('/home','Home::index', ['filter' => 'authenticatedUser']);
 $routes->get('/login','Connection::index');
 $routes->post('/login','Connection::attemptLogin');
 $routes->get('/addUser','AddUser::index',['filter' => 'authenticatedUser']);
-$routes->get('gestionAbonne','GestionAbonne::index',['filter' => 'authenticatedAdmin']);
-$routes->get('gestionLivre','GestionLivre::index',['filter' => 'authenticatedUser']);
+$routes->get('/gestionAbonne','GestionAbonne::index',['filter' => 'authenticatedAdmin']);
+$routes->get('/gestionLivre','GestionLivre::index',['filter' => 'authenticatedUser']);
