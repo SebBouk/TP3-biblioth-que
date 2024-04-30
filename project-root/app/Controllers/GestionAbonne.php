b81ba260335f87bd5765e529bd1019535a85fd6b
Sébastien <seb.boukobza@gmail.com>
2024-04-30 09:49:06 +0200

    route

--- project-root/app/Controllers/GestionAbonne.php
+++ project-root/app/Controllers/GestionAbonne.php
@@ -21,4 +21,22 @@ class GestionAbonne extends BaseController
 
         return $template;
     }
+        public function formulaireAdd(): string
+    {
+        $session = session();
+        $template =
+        view('templates/header.php',[
+        'loggedIn' => $session->get('loggedIn'),
+        'name' => $session->get('username')
+        ]).
+        view('add-user').
+        view('templates/footerHome.php');
+
+        return $template;
+    }
+    public function Add_User(){
+        $newUser = new \App\Models\Gestion_Abonne();
+        $values = $this->request->getPost(['nom_abonne','date_de_naissance','adresse_abonne','telephone_abonne','CSP_abonne']);
+        return redirect()->to('gestionAbonne');
+    }
 }
